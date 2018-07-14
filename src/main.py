62608508b236026aeb797f85a634847fc659edb4
Miquel Puig <miquelpuigpey@gmail.com>
2018-07-14 18:41:47 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import time
+from sense_hat import SenseHat
+
+
+def tick():
+    pass
+
+
+if __name__ == '__main__':
+    sense = SenseHat()
+
+    sense.show_message("Hello world!")
+
+    starttime = time.time()
+    while True:
+        tick()
+        time.sleep(60.0 - ((time.time() - starttime) % 60.0))
